cddbf55428c5cf49fc28bca4cb18506f3fb963ec
Photo Keri <photoker@su-a1.msk.21-school.ru>
2022-12-15 20:30:48 +0300

    fixes

--- s21_memcmp.c
+++ s21_memcmp.c
@@ -6,11 +6,13 @@ int s21_memcmp(const void* str1, const void* str2, s21_size_t n) {
     const char* s1 = str1;
     const char* s2 = str2;
     int res = 0;
-    for (s21_size_t i = 0; i < n && res == 0; i++) {
-        if ( *s1 < *s2 ) res--;
-        else if (*s1 > *s2) res++;
-        s1++;
-        s2++; 
+    if (s1 != s21_NULL && s2 != s21_NULL) {
+        for (s21_size_t i = 0; i < n && res == 0; i++) {
+            if ( *s1 < *s2 ) res--;
+            else if (*s1 > *s2) res++;
+            s1++;
+            s2++; 
+        }
     }
     return res;
 }
--- s21_strcmp.c
+++ s21_strcmp.c
@@ -1,6 +1,10 @@
 #include "s21_string.h"
 
+s21_size_t s21_strlen(const char *str) {
+  for (s21_size_t strLength = 0; str[strLength]; strLength++);
+  return strLength;
+}
 
 int s21_strcmp(const char *str1, const char *str2){
-
+    
 }
