eb1e3dac8f9c45582ab1ac0d7d741d582fcf7410
Shaman Antone <shamanan@su-a2.msk.21-school.ru>
2022-12-15 20:29:37 +0300

    idi

--- s21_strspn.c
+++ s21_strspn.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
-#include <string.h>
-int main(void) {
-    int len;
-    len = strspn("siht is a test", "siht");
-    printf ("%d", len);
-    return 0;
+#include "s21_string.h"
+// #include <string.h>
+// #include <stdio.h>
+
+s21_size_t s21_strspn(const char *str, const char *keys) {
+  size_t n;
+  const char* j;
+  for(n = 0; *str; str++, n++) {
+    for(j = keys; *j && *j != *str; j++);
+    if (!*j)
+      break;
+  }
+  return n;
 }
 
-// #include "../s21_string.h"
 
-// s21_size_t s21_strspn(const char *str, const char *keys) {
-
+// int main(void) {
+//     int len, gen;
+//     len = strspn("hff siht te sihtst siht ", "siht ");
+//     gen = s21_strspn("hff siht te sihtst siht ", "siht ");
+//     printf ("%d %d", len, gen);
+//     return 0;
 // }
