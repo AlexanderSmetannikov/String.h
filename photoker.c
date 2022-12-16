48e9875f0e354340f3799375f3cdd075419ae992
Photo Keri <photoker@at-h6.msk.21-school.ru>
2022-12-16 17:27:22 +0300

    photoker except error

--- photoker.c
+++ photoker.c
@@ -92,10 +92,64 @@ char *s21_strcat(char *dest, const char *src) {
 }
 
 char *s21_strncat(char *dest, const char *src, size_t n) {
-    while (*dest) dest++;
-    dest--;
-    while(*src && n--) *dest++ = *src++;
+    int len = 0;
+    while (*dest) {
+        dest++;
+        len++;
+    }
+    while(*src && n--) {
+        *dest++ = *src++;
+        len++;
+    }
+    *dest = '\0';
+    dest -= len; 
     return dest;
+}
+
+int is_delim(char c, const char* delim) {
+    int ans = 0;
+    while (*delim && !ans) {
+        if(c == *delim) ans = 1;
+        delim++;
+    }
+    return ans;
+}
+
+s21_size_t s21_strcspn(const char *str, const char *keys) {
+    s21_size_t res = 0;
+    while(!s21_strchr(keys, *str) && *str != '\0') {
+        str++;
+        res++;
+    }
+    return res;
+}
+
+char *s21_strtok(char *str, const char *delim) {
+    static char* back_str;
+    
+    if(!str) str = back_str;
+    if(!str) return NULL;
+    while(1) {
+        if(is_delim(*str, delim)) {
+            str++;
+            continue;
+        }
+        if(*str == '\0') return NULL; 
+        break;
+    }
+    char *ret = str;
+    while(1) {
+        if(*str == '\0') {
+            back_str = str;
+            return ret;
+        }
+        if(is_delim(*str, delim)) {
+            *str = '\0';
+            back_str = str + 1;
+            return ret;
+        }
+        str++;
+    }
 }
 
 int main() {
@@ -118,17 +172,40 @@ int main() {
 
     //const char s1[] = "atoms\0fdsf0\0";
     //char s2[] = "atoms\0abc";
-   const char s3[] = "atoms\0fdsf0\0";
-    char s4[] = "atoms\0abc";
+//    const char s3[] = "atom";
+//     char s4[] = "atom";
 
     // s21_strcpy(s2, s1);
     // strcpy(s4, s3);
     // printf("%s\n", s2);
     // printf("%s\n", s4);
+//     char app[1024]="вторая строка ";
+//    // Массив приемник данных
+//     char dst[1024]="первая строка";
+//     // s21_strcat(s2, s1);
+//     strncat(dst, app, 2);
+//     // printf("%s\n", s2);
+//     printf("%s\n", dst);
 
-    // s21_strcat(s2, s1);
-    strcat(s4, s3);
-    // printf("%s\n", s2);
-    printf("%s\n", s4);
+    char str [44]="test1/test2/test3/test4,/test5";
+   // Набор символов, которые должны входить в искомый сегмент
+   char sep [10]="/,";
+   // Переменная, в которую будут заноситься начальные адреса частей
+   // строки str
+   char *istr;
+
+   printf ("Исходная строка: %s\n", str);
+   printf ("Результат разбиения:\n");
+   // Выделение первой части строки
+   istr = s21_strtok (str,sep);
+
+   // Выделение последующих частей
+   while (istr != NULL)
+   {
+      // Вывод очередной выделенной части
+      printf ("%s\n",istr);
+      // Выделение очередной части строки
+      istr = s21_strtok(NULL,sep);
+   }
     return 0;
 }
