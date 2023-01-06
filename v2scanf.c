32d7df54306edd5ebdea6f27c6e9959bc1dd6089
Shaman Antone <shamanan@su-c3.msk.21-school.ru>
2023-01-06 14:48:03 +0300

    scanV3

--- v2scanf.c
+++ v2scanf.c
@@ -14,7 +14,7 @@ typedef long long ll;
 typedef struct Token {
   int width;
   int length;
-  int sign;
+  char sign;
 } Token;
 
 // s21_size_t s21_strlen(const char* str) {
@@ -36,20 +36,50 @@ int s21_getchar(const char* str, char* c) {
   return result;
 }
 
-int s21_putnumber(char* buf, ll num, Token* tok) {
+// int s21_putnumber(char* buf, ll num, Token* tok) {
+//   int res = 0;
+//   if (tok->sign) num *= -1;
+//   res += flag_implementation(buf, tok);
+//   ll div = 1;
+//   while (num / div != 0) div *= 10;
+//   div /= 10;
+//   ll ostatok = 0;
+//   while (num % div >= 0) {
+//     ostatok = num / div;
+//     res += s21_putchar(buf, (char)('0' + ostatok));
+//     num = num % div;
+//     div /= 10;
+//     if (div == 0) break;
+//   }
+//   return res;
+// }
+
+int is_spec(char c) {
+  return (c == 'c' || c == 'd' || c == 'i' || c == 'f' || c == 's' ||
+          c == 'u' || c == '%' || c == 'G' || c == 'g' || c == 'o' || c == 'x' ||
+          c = 'X' || c == 'p' || c == 'n' || c == 'e' || c == 'E');
+}
+
+int token_config(char** fmt, Token* tk, va_list* args) {
   int res = 0;
-  if (tok->sign) num *= -1;
-  res += flag_implementation(buf, tok);
-  ll div = 1;
-  while (num / div != 0) div *= 10;
-  div /= 10;
-  ll ostatok = 0;
-  while (num % div >= 0) {
-    ostatok = num / div;
-    res += s21_putchar(buf, (char)('0' + ostatok));
-    num = num % div;
-    div /= 10;
-    if (div == 0) break;
+  int len = 0;
+  while (**fmt) {
+    if (**fmt == '*') { 
+      tk -> width = 
+    }
+    if (is_digit(*fmt)) {
+      tk->width = s21_stoi(fmt);
+      res += numLength(tk->width);
+    }
+    if (**fmt == '%') {
+      res = 0;
+      break;
+    }
+    if (is_spec(**fmt)) {
+      (*fmt)--;
+      break;
+    }
+    (*fmt)++;
   }
   return res;
 }
@@ -109,134 +139,127 @@ int s21_getstring(const char* str, char* string) {
 //           c == '\r' || c == '\t' || c == '\v');
 // }
 
-// int spec_config(const char* format, Token* tok) {
+// int spec_config(const char* fmt, Token* tok) {
 //   int res = 0;
 //   int len = 0;
-//   while (*format) {
-//     if (*format == '+') {
+//   while (*fmt) {
+//     if (*fmt == '+') {
 //       tok->flag |= flag_plus;
 //       res++;
 //     }
-//     if (*format == '-') {
+//     if (*fmt == '-') {
 //       tok->flag |= flag_minus;
 //       res++;
 //     }
-//     if (*format == ' ') {
+//     if (*fmt == ' ') {
 //       tok->flag |= flag_space;
 //       res++;
 //     }
-//     if (is_digit(*format)) {
-//       tok->width = s21_stoi(format);
+//     if (is_digit(*fmt)) {
+//       tok->width = s21_stoi(fmt);
 //       res += numLength(tok->width);
 //     }
-//     if (*format == '.') {
-//       format++;
+//     if (*fmt == '.') {
+//       fmt++;
 //       res++;
-//       if (is_digit(*format)) {
-//         tok->precision = s21_stoi(format);
+//       if (is_digit(*fmt)) {
+//         tok->precision = s21_stoi(fmt);
 //         len = numLength(tok->precision);
 //         res += len;
-//         format += len - 1;
+//         fmt += len - 1;
 //       } else
 //         tok->precision = 0;
 //     }
-//     if (*format == '%') {
+//     if (*fmt == '%') {
 //       res = 0;
 //       break;
 //     }
-//     if (is_spec(*format)) {
-//       format--;
+//     if (is_spec(*fmt)) {
+//       fmt--;
 //       break;
 //     }
-//     format++;
+//     fmt++;
 //   }
 //   return res;
 // }
 
-// void reset_specs(Token* tok) {
-//   tok->precision = 0;
-//   tok->flag = 0;
-//   tok->width = 0;
-//   tok->length = 0;
-//   tok->sign = 0;
-// }
-
-// void fill_buff(char* buf, int n) {
-//   for (int i = 0; i < n; i++) s21_putchar(buf, ' ');
-// }
+void reset_specs(Token* tok) {
+  tok->width = 0;
+  tok->length = 0;
+  tok->sign = 0;
+}
 
 int s21_sscanf(const char* str, const char* format, ...) {
-  //Token tok = {0};
+  Token tok = {0};
+  char *data = (char*)str,
+      *fmt = (char*)format;
   va_list args;
-  va_start(args, format);
+  va_start(args, fmt);
   int res = *str ? 0 : -1;
-  //int move = 0;
 
-  while (*format) {
+  while (*fmt) {
     if (!*str) break;
-    if (*format == '%') {
-      format++;
-      //reset_specs(&tok);
-      //move = spec_config(format, &tok);
-      //format += move;
-      if (*format == 'c') {
-        res += s21_getchar(str, (char*)va_arg(args, char*));
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 'c') {
+        res += s21_getchar(data, (char*)va_arg(args, char*));
         str++;
       }
     }
-      if (*format == 's') {
+      if (*fmt == 's') {
         char* string = va_arg(args, char*);
-        res += s21_getstring(str, string);
+        res += s21_getstring(data, string);
         break;
       }
-      if (*format == 'd') {
-        int* num = va_arg(args, int*);
-        // if (num < 0) tok.sign = -1;
-        res += s21_getnumber(str, num, &tok);
-      }
-      // if (*format == 'i') {
+      // if (*fmt == 'd') {
       //   int* num = va_arg(args, int*);
       //   // if (num < 0) tok.sign = -1;
       //   res += s21_getnumber(str, num, &tok);
       // }
-    //   if (*format == 'u') {
+      // if (*fmt == 'i') {
+      //   int* num = va_arg(args, int*);
+      //   // if (num < 0) tok.sign = -1;
+      //   res += s21_getnumber(str, num, &tok);
+      // }
+    //   if (*fmt == 'u') {
     //     ll num = va_arg(args, unsigned int);
     //     if (num < 0) tok.sign = -1;
     //     res += s21_putnumber(buf, num, &tok);
     //   }
-    //   if (*format == 'f') {
+    //   if (*fmt == 'f') {
     //     double num = va_arg(args, double);
     //     if (num < 0) tok.sign = -1;
     //     res += s21_putfloat(buf, num, &tok);
     //   }
-    //   if (*format == '%') {
-    //     res += s21_putchar(buf, *format);
+    //   if (*fmt == '%') {
+    //     res += s21_putchar(buf, *fmt);
     //   }
     // } else
-    //   res += s21_putchar(buf, *format);
-    format++;
+    //   res += s21_putchar(buf, *fmt);
+    fmt++;
   }
   va_end(args);
   return res;
 }
 
 int main() {
-    char buf11[80];
+      
+      char buf11[80];
     char buf21[80];
 
-     char buf12;
+      char buf12;
     char buf22;
 
-     char buf13;
+      char buf13;
     char buf23;
 
-    int res_1 = s21_sscanf("f234", "%s%c%c", buf11, &buf12, &buf13);
-    int res_1_orig = sscanf("f234", "%s%c%c", buf21, &buf22, &buf23);
+    int res_1 = s21_sscanf("f 234", "%s%c%c", buf11, &buf12, &buf13);
+    int res_1_orig = sscanf("fdgdgdg234", "%3s%c%c", buf21, &buf22, &buf23);
     
 
     printf("res s21 = %d | res_orig = %d\n", res_1, res_1_orig);
     printf("s21_sscanf: %s %c %c\n", buf11, buf12, buf13);
-    printf("ori_sscanf: %s %c %c\n", buf21, buf22, buf23);
+    printf("ori_sscanf: %s %c%c\n", buf21, buf22, buf23);
 
 
     //     char buf11;
